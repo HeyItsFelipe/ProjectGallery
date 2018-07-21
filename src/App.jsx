88f0c40ffcc41a3dc3d19ffa6921e485f3479211
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-07-20 17:39:22 -0700

    Initial setup for ProjectGallery.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>ProjectGallery</h2>
+        <input />
+        <button>Search</button>
+      </div>
+    );
+  }
+}
+
+export default App;
