2d7147f3daf8db724d6617c23edb0407be59dfb4
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-07-21 16:29:58 -0700

    Adds images and projectData.json.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Header from './Header.jsx';
 import './App.css';
+import preload from './projectData.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ''
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(event) {
+    this.setState({
+      search: event.target.value
+    });
+  }
+
   render() {
+    let result = preload['projects'].filter((element, index, array) => {
+      return element['title'].toUpperCase().indexOf(this.state.search.toUpperCase()) > -1;
+    });
     return (
       <div>
         <Header />
+        <pre>
+          <code>{JSON.stringify(result, null, 4)}</code>
+        </pre>
       </div>
     );
   }
