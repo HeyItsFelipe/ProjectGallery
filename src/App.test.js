8d833061497c2374e3c19e6497496157be7b9e4d
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-07-25 22:22:42 -0700

    Sets up enzyme testing.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './Header';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<App />);
+  //console.log(wrapper.find(Header));
+  expect(wrapper.find(Header).length).toBe(1);
+  //const div = document.createElement('div');
+  //ReactDOM.render(<App />, div);
+  //ReactDOM.unmountComponentAtNode(div);
 });
