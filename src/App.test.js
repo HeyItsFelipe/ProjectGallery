d9b99e6476d8ab0a08d3791c925f87cccd2462c7
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-07-26 22:34:32 -0700

    Adds tests to App component.

--- src/App.test.js
+++ src/App.test.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Header from './Header';
+import Gallery from './Gallery';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
+describe('App component', () => {
   const wrapper = shallow(<App />);
-  //console.log(wrapper.find(Header));
-  expect(wrapper.find(Header).length).toBe(1);
-  //const div = document.createElement('div');
-  //ReactDOM.render(<App />, div);
-  //ReactDOM.unmountComponentAtNode(div);
+
+  it('it contains a Header component', () => {
+    expect(wrapper.find(Header).length).toBe(1);
+  });
+
+  it('it contains a Gallery component', () => {
+    expect(wrapper.find(Gallery).length).toBe(1);
+  });
 });
